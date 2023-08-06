ae7a5b811fff3b4973a6aba84c6b9e303a7227ee
Pavel Klečanský <klecanskypavel@gmail.com>
2023-08-06 14:58:06 +0200

    Add Github actions and add context loading test with test containers.

---
+++ backend/src/test/java/cz/klecansky/projectmanagement/ContainersConfig.java
@@ -0,0 +1,27 @@
+package cz.klecansky.projectmanagement;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class ContainersConfig {
+    @Bean
+    @ServiceConnection
+    public PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>("postgres:15.2-alpine");
+    }
+
+    @Bean
+    public GenericContainer mailhogContainer(DynamicPropertyRegistry registry) {
+        GenericContainer container = new GenericContainer("mailhog/mailhog").withExposedPorts(1025);
+        registry.add("smtp.server", container::getHost);
+        registry.add("smtp.port", container::getFirstMappedPort);
+        registry.add("smtp.username", () -> "test@test.com");
+        registry.add("smtp.password", () -> "password");
+        return container;
+    }
+}
