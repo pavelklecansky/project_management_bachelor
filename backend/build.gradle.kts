08756e0783b1a46b157c6325ccc678866052e539
Pavel Klečanský <klecanskypavel@gmail.com>
2023-08-31 20:15:32 +0200

    Add liquibase migration.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     implementation("org.projectlombok:lombok:1.18.22")
     implementation("org.modelmapper:modelmapper:2.4.4")
+    implementation("org.liquibase:liquibase-core")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
     runtimeOnly("org.postgresql:postgresql")
